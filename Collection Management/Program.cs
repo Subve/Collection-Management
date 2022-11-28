9829939148091b7469ef1aee11611a3c9444d4c7
Subve <60174799+Subve@users.noreply.github.com>
2022-11-28 10:38:37 +0100

    Add project files.

---
+++ Collection Management/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Collection_Management
+{
+    internal class Program
+    {
+        public const string PATH_WAY = @"C:\Users\milos\Documents\Collection_Manager\ImportFile.xlsx";
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Your Collection Manager");
+            Console.WriteLine("Please choose your action : ");
+            Console.WriteLine("1. Add Item");
+            Console.WriteLine("2. Update Item");
+            Console.WriteLine("3. Delete Item");
+            Console.WriteLine("4. Show Item");
+
+            string? choose=Console.ReadLine();
+            int chosenOption = 0;
+            Int32.TryParse(choose, out chosenOption);
+
+            Console.WriteLine($"You have choosen option number: {chosenOption}");
+        }
+    }
+}
