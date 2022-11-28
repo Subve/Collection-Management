eb076a31f8b8b9364763ba1eb10bff5fe4e3204f
Subve <60174799+Subve@users.noreply.github.com>
2022-11-29 00:39:46 +0100

    MenuManager- AddNewAction and ShowMenuAction

--- Collection Management/Menu.cs
+++ Collection Management/Menu.cs
@@ -8,5 +8,10 @@ namespace Collection_Management
 {
     public class Menu
     {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+
+        public int State { get; set; }
     }
 }
--- Collection Management/MenuManager.cs
+++ Collection Management/MenuManager.cs
@@ -8,5 +8,41 @@ namespace Collection_Management
 {
     public class MenuManager
     {
+        List<Menu> menuList = new();
+        public MenuManager() { }
+        public void AddNewAction(int id,string name,string description, int state)
+        {
+            Menu menuAction=new Menu() {Id=id,Name=name,Description=description, State=state};
+            menuList.Add(menuAction);
+        }
+        public void ShowMenuAction(int state) 
+        {
+            //Show actions of state
+            switch (state)
+            {
+                case 1:
+                    {
+                        foreach (Menu menu in menuList)
+                        {
+                           Console.WriteLine(menu.Description);
+
+                        }
+                        break;
+                    }
+                case 2:
+                    {
+                        foreach (Menu menu in menuList)
+                        {
+                            Console.WriteLine(menu.Description);
+
+                        }
+                        break;
+                    }
+                default:
+                    break;
+            }
+        }
+
+        
     }
 }
